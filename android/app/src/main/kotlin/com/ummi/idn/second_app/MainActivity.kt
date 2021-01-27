7b2c4e1fece0cdaf833c6c88117ac5ce56bfab71
Ummi Kolbia <miikolbia@gmail.com>
2021-01-27 15:30:17 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/ummi/idn/second_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ummi.idn.second_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/home_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        leading:
+        IconButton(
+          icon: Icon(Icons.menu,
+            color: Colors.white,),),
+        title: Text("Student App"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+          ),
+        IconButton(
+            icon: Icon(Icons.settings,
+              color: Colors.white,)
+        )
+        ],
+      ),
+      body:
+      Container(
+        margin: EdgeInsets.only(left: 20.0, top: 20.0),
+          width: 100.0,
+          height: 50.0,
+          decoration: BoxDecoration(color: Colors.blue, boxShadow: [
+            BoxShadow(color: Colors.grey, offset:Offset(6,4), blurRadius: 10),
+            ],
+          ),
+          child: Text("Selamat belajar flutter !")),
+    floatingActionButton: FloatingActionButton(
+      child: Icon(
+        Icons.favorite,
+        color: Colors.white,
+      ),
+      ),
+    );
+  }
+}
+
